3b301a3e7c964327c149511e587c96298a7619f7
Julien Dudek <julien.dudek@gmail.com>
2020-11-16 14:05:09 +0100

    #12 ajout converter vierge
    en attente ajout dependance

---
+++ src/main/java/com/aclab/dne/converter/UeConverter.java
@@ -0,0 +1,12 @@
+package com.aclab.dne.converter;
+
+import com.aclab.dne.dto.UeDTO;
+import com.aclab.dne.model.Ue;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class UeConverter {
+
+    
+}
