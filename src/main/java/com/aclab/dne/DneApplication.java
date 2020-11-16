35a3fb4acafacbefc8783ecbc88380088216dcb4
Morgan <47953421+MorganLmd@users.noreply.github.com>
2020-11-16 11:02:33 +0100

    Mise en place du projet Spring Boot

---
+++ src/main/java/com/aclab/dne/DneApplication.java
@@ -0,0 +1,13 @@
+package com.aclab.dne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aclab/dne/DneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aclab.dne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
