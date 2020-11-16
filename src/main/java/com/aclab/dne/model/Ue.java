c24f50df2a68df75a2d867068f4324965c2e74c5
Julien Dudek <julien.dudek@gmail.com>
2020-11-16 14:58:52 +0100

    #12

--- src/main/java/com/aclab/dne/model/Ue.java
+++ src/main/java/com/aclab/dne/model/Ue.java
@@ -1,11 +1,17 @@
 package com.aclab.dne.model;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import lombok.Data;
+
+import javax.persistence.*;
 
 @Entity
+@Data
+@Table
 public class Ue {
     @Id
-    int idUE;
-    String intitule;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idUE;
+
+    private String intitule;
+
 }
