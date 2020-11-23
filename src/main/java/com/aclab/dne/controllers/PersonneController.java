eac91846b61b0603b86d2ba22cd9f9106ba80ec0
Morgan <47953421+MorganLmd@users.noreply.github.com>
2020-11-23 11:58:30 +0100

    Update PersonneController.java

--- src/main/java/com/aclab/dne/controllers/PersonneController.java
+++ src/main/java/com/aclab/dne/controllers/PersonneController.java
@@ -4,11 +4,5 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PersonneController {
-<<<<<<< HEAD
 
-
-
-=======
-    
->>>>>>> ab06b7f87f4ae386b6464fdf2e5d7158ef35b0f8
 }
