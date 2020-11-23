e87d5cd2135217f31516080c8273659a38e1488d
PierreD <pierre.darcas@hotmail.fr>
2020-11-23 11:02:03 +0100

    Model : personne

--- src/main/java/com/aclab/dne/model/Personne.java
+++ src/main/java/com/aclab/dne/model/Personne.java
@@ -1,4 +1,23 @@
 package com.aclab.dne.model;
 
-public class Personne {
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Data
+@Table
+public class Personne implements Serializable {
+
+    @Id
+    @GeneratedValue
+    private Long idPersonne;
+    private String nom;
+    private String prenom;
+
+
 }
