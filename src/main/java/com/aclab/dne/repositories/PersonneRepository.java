975eee5094d9ff5db59c837c528b6038ac77c539
Julien Dudek <julien.dudek@gmail.com>
2020-11-23 22:35:09 +0100

    #1 ajout CrudRepository
    Ajout extends

--- src/main/java/com/aclab/dne/repositories/PersonneRepository.java
+++ src/main/java/com/aclab/dne/repositories/PersonneRepository.java
@@ -1,4 +1,7 @@
 package com.aclab.dne.repositories;
 
-public interface PersonneRepository {
+import com.aclab.dne.model.Personne;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PersonneRepository extends CrudRepository<Personne,Integer> {
 }
