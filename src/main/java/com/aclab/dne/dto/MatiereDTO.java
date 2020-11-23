c3bf030e215b994c27289352d0dbc30c1c2e0e4d
Julien Dudek <julien.dudek@gmail.com>
2020-11-23 22:32:24 +0100

    #23 DTO + Repo

--- src/main/java/com/aclab/dne/dto/MatiereDTO.java
+++ src/main/java/com/aclab/dne/dto/MatiereDTO.java
@@ -1,2 +1,88 @@
-package com.aclab.dne.dto;public class MatiereDTO {
+package com.aclab.dne.dto;
+
+public class MatiereDTO {
+    private int idMatiere;
+    private String codeMatiere;
+    private int quotaHeure;
+    private int creditECTS;
+    private String intitule;
+    private String description;
+    private int semestre;
+
+    public MatiereDTO(String codeMatiere, String intitule) {
+        this.codeMatiere = codeMatiere;
+        this.intitule = intitule;
+    }
+    public MatiereDTO(){
+        super();
+    }
+
+    public int getIdMatiere() {
+        return idMatiere;
+    }
+
+    public void setIdMatiere(int idMatiere) {
+        this.idMatiere = idMatiere;
+    }
+
+    public String getCodeMatiere() {
+        return codeMatiere;
+    }
+
+    public void setCodeMatiere(String codeMatiere) {
+        this.codeMatiere = codeMatiere;
+    }
+
+    public int getQuotaHeure() {
+        return quotaHeure;
+    }
+
+    public void setQuotaHeure(int quotaHeure) {
+        this.quotaHeure = quotaHeure;
+    }
+
+    public int getCreditECTS() {
+        return creditECTS;
+    }
+
+    public void setCreditECTS(int creditECTS) {
+        this.creditECTS = creditECTS;
+    }
+
+    public String getIntitule() {
+        return intitule;
+    }
+
+    public void setIntitule(String intitule) {
+        this.intitule = intitule;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getSemestre() {
+        return semestre;
+    }
+
+    public void setSemestre(int semestre) {
+        this.semestre = semestre;
+    }
+
+    @Override
+    public String toString() {
+        return "MatiereDTO{" +
+                "idMatiere=" + idMatiere +
+                ", codeMatiere='" + codeMatiere + '\'' +
+                ", quotaHeure=" + quotaHeure +
+                ", creditECTS=" + creditECTS +
+                ", intitule='" + intitule + '\'' +
+                ", description='" + description + '\'' +
+                ", semestre=" + semestre +
+                '}';
+    }
 }
--- src/main/java/com/aclab/dne/repositories/MatiereRepository.java
+++ src/main/java/com/aclab/dne/repositories/MatiereRepository.java
@@ -1,2 +1,9 @@
-package com.aclab.dne.repositories;public interface MatiereRepository {
+package com.aclab.dne.repositories;
+
+import com.aclab.dne.model.Matiere;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatiereRepository extends CrudRepository<Matiere,Integer> {
 }
