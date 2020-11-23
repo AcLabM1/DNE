f7e0d606a7255aae74c49c4b03cf514f3ad5117d
Julien Dudek <julien.dudek@gmail.com>
2020-11-23 10:37:50 +0100

    #12 pour test UE
    divers pour test

--- src/main/java/com/aclab/dne/repositories/UeRepository.java
+++ src/main/java/com/aclab/dne/repositories/UeRepository.java
@@ -2,7 +2,8 @@ package com.aclab.dne.repositories;
 
 import com.aclab.dne.model.Ue;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface UeRepository extends CrudRepository<Ue,Integer> {
 }
