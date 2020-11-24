3477e4fe6dc64c9880fdb8fe443e447c4f9d834d
Morgan <47953421+MorganLmd@users.noreply.github.com>
2020-11-24 14:28:33 +0100

    #3 implementation interneUniv entity + dto + converter reste les liaisons hibernate

---
+++ src/main/java/com/aclab/dne/dto/InterneUnivDTO.java
@@ -0,0 +1,23 @@
+package com.aclab.dne.dto;
+
+public class InterneUnivDTO {
+
+    private Long interneUnivId;
+    private String email;
+
+    public Long getInterneUnivId() {
+        return interneUnivId;
+    }
+
+    public void setInterneUnivId(Long interneUnivId) {
+        this.interneUnivId = interneUnivId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
