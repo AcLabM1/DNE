c9eaf96e8cea6a4ee24e5399fef9a2977dd48f96
Julien Dudek <julien.dudek@gmail.com>
2020-11-27 00:36:43 +0100

    #6 nettoyage imports

--- src/main/java/com/aclab/dne/repositories/EmployeRepository.java
+++ src/main/java/com/aclab/dne/repositories/EmployeRepository.java
@@ -3,7 +3,6 @@ package com.aclab.dne.repositories;
 import com.aclab.dne.model.Employe;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 
 @Repository
 public interface EmployeRepository extends CrudRepository<Employe,Long> {
