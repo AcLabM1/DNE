a292402c50d4202cb267ec35dfa503a274e8c74c
Julien Dudek <julien.dudek@gmail.com>
2020-12-02 10:13:52 +0100

    #7 delta

--- src/main/java/com/aclab/dne/converter/ResponsableFormationConverter.java
+++ src/main/java/com/aclab/dne/converter/ResponsableFormationConverter.java
@@ -1,2 +1,31 @@
-package com.aclab.dne.converter;public class ResponsableFormationConverter {
+package com.aclab.dne.converter;
+
+import com.aclab.dne.dto.ResponsableFormationDTO;
+import com.aclab.dne.model.ResponsableFormation;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class ResponsableFormationConverter {
+
+    public ResponsableFormationDTO entityToDTO(ResponsableFormation responsableFormation){
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(responsableFormation,ResponsableFormationDTO.class);
+    }
+
+    public List<ResponsableFormationDTO> entityToDTO(List<ResponsableFormation> responsableFormations){
+        return responsableFormations.stream().map(this::entityToDTO).collect(Collectors.toList());
+    }
+
+    public ResponsableFormation dtoToEntity(ResponsableFormationDTO responsableFormationDTO){
+        ModelMapper mapper = new ModelMapper();
+        return mapper.map(responsableFormationDTO,ResponsableFormation.class);
+    }
+
+    public List<ResponsableFormation> dtoToEntity(List<ResponsableFormationDTO> responsableFormationDTOS){
+        return responsableFormationDTOS.stream().map(this::dtoToEntity).collect(Collectors.toList());
+    }
 }
--- src/main/java/com/aclab/dne/repositories/ResponsableFormationRepository.java
+++ src/main/java/com/aclab/dne/repositories/ResponsableFormationRepository.java
@@ -1,2 +1,9 @@
-package com.aclab.dne.repositories;public class ResponsableFormationRepository {
+package com.aclab.dne.repositories;
+
+import com.aclab.dne.model.ResponsableFormation;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ResponsableFormationRepository extends CrudRepository<ResponsableFormation,Long> {
 }
