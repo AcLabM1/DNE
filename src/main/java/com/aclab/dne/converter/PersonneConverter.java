a6140589b8956eb5e053887426f44b27ec418358
Pierre <pierre.darcas@lacatholille.fr>
2020-12-02 09:20:35 +0100

    delete code from personneConverter

--- src/main/java/com/aclab/dne/converter/PersonneConverter.java
+++ src/main/java/com/aclab/dne/converter/PersonneConverter.java
@@ -10,43 +10,5 @@ import java.util.stream.Collectors;
 
 @Component
 public class PersonneConverter {
-
-    //should be empty because Personne is abstract
-    /**
-     * Passage de Personne en PersonneDTO
-     * @param personne
-     * @return
-     */
-    public PersonneDTO entityToDto(Personne personne){
-        ModelMapper mapper = new ModelMapper();
-        return mapper.map(personne,PersonneDTO.class);
-    }
-
-    /**
-     * Passe une List Personne en une List de Personne DTO
-     * @param personnes List PersonneDTO
-     * @returne
-     **/
-    public List<PersonneDTO> entityToDto(List<Personne> personnes){
-        return personnes.stream().map(this::entityToDto).collect(Collectors.toList());
-    }
-
-    /**
-     * Passage PersonneDTO en Personne
-     * @param personneDTO
-     * @return
-     **/
-    public Personne dtoToEntity(PersonneDTO personneDTO){
-        ModelMapper mapper = new ModelMapper();
-        return mapper.map(personneDTO,Personne.class);
-    }
-
-    /**
-     * Passage List PersonneDTO en List Personne
-     * @param personneDTOS List Personne
-     * @return
-     */
-    public List<Personne> dtoToEntity(List<PersonneDTO> personneDTOS){
-        return personneDTOS.stream().map(this::dtoToEntity).collect(Collectors.toList());
-    }
+    
 }
