b7a6f927de789f34585ee9afc99e8632e5aedfe0
Julien Dudek <julien.dudek@gmail.com>
2021-01-22 00:46:03 +0100

    #68 imports

--- src/main/java/com/aclab/dne/services/PersonneService.java
+++ src/main/java/com/aclab/dne/services/PersonneService.java
@@ -4,7 +4,6 @@ import com.aclab.dne.converter.*;
 import com.aclab.dne.model.Personne;
 import com.aclab.dne.repositories.*;
 import lombok.AllArgsConstructor;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
