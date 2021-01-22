373614ccb4dda6f8c4a5c5a11bccadf6c186e239
Morgan Lombard <morgan.lombard@protonmail.com>
2021-01-22 22:47:08 +0100

    #56 Création de l'entité Communication

---
+++ src/main/java/com/aclab/dne/model/CommunicationId.java
@@ -0,0 +1,18 @@
+package com.aclab.dne.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class CommunicationId implements Serializable {
+
+    private Long idEtudiant;
+    private Long idPromotion;
+    private Long idEmploye;
+    private Long idCommunication;
+}
