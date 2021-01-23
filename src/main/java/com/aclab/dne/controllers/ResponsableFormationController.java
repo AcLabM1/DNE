6461c100da4a2bf508e0a82978ee10d1a776af32
Pierre <pierre.darcas@lacatholille.fr>
2021-01-23 15:45:51 +0100

    :hammer: ResponsableFormationController

--- src/main/java/com/aclab/dne/controllers/ResponsableFormationController.java
+++ src/main/java/com/aclab/dne/controllers/ResponsableFormationController.java
@@ -1,37 +1,35 @@
 package com.aclab.dne.controllers;
 
 import com.aclab.dne.configuration.SwaggerConfig;
-import com.aclab.dne.converter.ResponsableFormationConverter;
 import com.aclab.dne.dto.ResponsableFormationDTO;
-import com.aclab.dne.model.ResponsableFormation;
-import com.aclab.dne.repositories.ResponsableFormationRepository;
+import com.aclab.dne.services.ResponsableFormationService;
 import io.swagger.annotations.Api;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(path = "/responsables")
 @Api(tags = { SwaggerConfig.RESPONSABLE })
 public class ResponsableFormationController {
 
-    private static final Logger LOG = LoggerFactory.getLogger(ResponsableFormationController.class);
-    private final ResponsableFormationConverter responsableFormationConverter;
-    private final ResponsableFormationRepository responsableFormationRepository;
+    private final ResponsableFormationService responsableFormationService;
 
-    @Autowired
-    public ResponsableFormationController(ResponsableFormationConverter responsableFormationConverter, ResponsableFormationRepository responsableFormationRepository) {
-        this.responsableFormationConverter = responsableFormationConverter;
-        this.responsableFormationRepository = responsableFormationRepository;
+    public ResponsableFormationController(ResponsableFormationService responsableFormationService) {
+        this.responsableFormationService = responsableFormationService;
     }
 
     @GetMapping
     public List<ResponsableFormationDTO> findAll(){
-        return this.responsableFormationConverter.entityToDTO((List<ResponsableFormation>) this.responsableFormationRepository.findAll());
+        try {
+            return this.responsableFormationService.findAllResponsableFormations();
+        }catch (NoSuchElementException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
