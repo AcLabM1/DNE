bc15493264b6099a289a2fb3ac171112c39f0e43
Pierre <pierre.darcas@lacatholille.fr>
2021-01-23 18:12:41 +0100

    📦 DiplomeService

--- src/main/java/com/aclab/dne/controllers/DiplomeController.java
+++ src/main/java/com/aclab/dne/controllers/DiplomeController.java
@@ -1,14 +1,21 @@
 package com.aclab.dne.controllers;
 
 import com.aclab.dne.configuration.SwaggerConfig;
-import com.aclab.dne.converter.DiplomeConverter;
-import com.aclab.dne.repositories.DiplomeRepository;
+import com.aclab.dne.dto.DiplomeDTO;
+import com.aclab.dne.dto.PromotionDTO;
+import com.aclab.dne.services.DiplomeService;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(path = "/diplomes")
@@ -16,12 +23,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class DiplomeController {
 
     private static final Logger LOG = LoggerFactory.getLogger(DiplomeController.class);
-    private final DiplomeConverter diplomeConverter;
-    private final DiplomeRepository diplomeRepository;
+    private final DiplomeService diplomeService;
 
-    @Autowired
-    public DiplomeController(DiplomeConverter diplomeConverter, DiplomeRepository diplomeRepository) {
-        this.diplomeConverter = diplomeConverter;
-        this.diplomeRepository = diplomeRepository;
+
+    public DiplomeController(DiplomeService diplomeService) {
+        this.diplomeService = diplomeService;
+    }
+
+    @GetMapping
+    @ApiOperation(value = "Retourne la liste des diplomes.")
+    public List<DiplomeDTO> findAll(){
+        LOG.debug("IN");
+        try {
+                return this.diplomeService.findAllDiplomes();
+        }catch (NoSuchElementException e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
     }
 }
